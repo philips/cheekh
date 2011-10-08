c11669d429e22148c6a4716ff7fa8b4d0c21ef47
Brandon Philips <brandon@ifup.org>
2011-10-07 18:16:20 -0700

    cheekh: initial commit

---
+++ hello.py
@@ -0,0 +1,63 @@
+import select
+import sys
+import pybonjour
+from netgrowl import *
+
+regtype  = "_growl._tcp"
+timeout  = 1
+hosts = []
+
+def growl_test(name, port):
+    addr = (name, GROWL_UDP_PORT)
+    s = socket(AF_INET,SOCK_DGRAM)
+    p = GrowlRegistrationPacket(password="reachforthestars")
+    p.addNotification()
+    s.sendto(p.payload(), addr)
+
+    p = GrowlNotificationPacket(priority=-2, password="reachforthestars", title="Hello Reach")
+    s.sendto(p.payload(), addr)
+
+def resolve_callback(sdRef, flags, interfaceIndex, errorCode, fullname,
+                     hosttarget, port, txtRecord):
+    if errorCode == pybonjour.kDNSServiceErr_NoError:
+        if not hosttarget in hosts:
+            growl_test(hosttarget, port)
+            hosts.append(hosttarget)
+
+def browse_callback(sdRef, flags, interfaceIndex, errorCode, serviceName,
+                    regtype, replyDomain):
+    if errorCode != pybonjour.kDNSServiceErr_NoError:
+        return
+
+    if not (flags & pybonjour.kDNSServiceFlagsAdd):
+        print 'Service removed'
+        return
+
+    resolve_sdRef = pybonjour.DNSServiceResolve(0,
+                                                interfaceIndex,
+                                                serviceName,
+                                                regtype,
+                                                replyDomain,
+                                                resolve_callback)
+
+    try:
+       ready = select.select([resolve_sdRef], [], [], timeout)
+       if resolve_sdRef not in ready[0]:
+           print 'Resolve timed out'
+       pybonjour.DNSServiceProcessResult(resolve_sdRef)
+    finally:
+        resolve_sdRef.close()
+
+
+browse_sdRef = pybonjour.DNSServiceBrowse(regtype = regtype,
+                                          callBack = browse_callback)
+
+try:
+    for n in range(0, 1):
+        # Only want to notify one host anyways
+        if len(hosts) > 0: break
+        ready = select.select([browse_sdRef], [], [], 1)
+        if browse_sdRef in ready[0]:
+            pybonjour.DNSServiceProcessResult(browse_sdRef)
+finally:
+    browse_sdRef.close()
